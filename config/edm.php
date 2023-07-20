84aeaeb822aade12b9952a550413f33bb9e4bb5e
ATAKAN ATICI <atakanatici0@gmail.com>
2023-07-21 01:41:04 +0300

    edit config file

--- config/edm.php
+++ config/edm.php
@@ -2,8 +2,19 @@
 
 // config for AtakanAtici/EDM
 return [
+    'edm_webservice' => env('EDM_WEBSERVICE', 'https://test.edmbilisim.com.tr/EFaturaEDM21ea/EFaturaEDM.svc?singleWsdl'),
+    'edm_username' => env('EDM_USERNAME', 'Datanet'),
+    'edm_password' => env('EDM_PASSWORD', '1234567'),
+    
     'senaryo' => [
         'TICARIFATURA' => 'Ticari Fatura',
         'TEMELFATURA' => 'Temel Fatura',
     ],
+    "fatura_tur" => [
+        'SATIS' => 'Satış',
+    ],
+    "cari_tur" => [
+        "TUZELKISI" => "Tüzel Kişi",
+        "GERCEKKISI" => "Gerçek Kişi",
+    ]
 ];

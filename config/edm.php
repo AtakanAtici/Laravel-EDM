81a3c57c3e22ffca813a8448264aaf1e8e1c53a5
AtakanAtici <AtakanAtici@users.noreply.github.com>
2023-07-20 22:41:34 +0000

    Fix styling

--- config/edm.php
+++ config/edm.php
@@ -5,16 +5,16 @@ return [
     'edm_webservice' => env('EDM_WEBSERVICE', 'https://test.edmbilisim.com.tr/EFaturaEDM21ea/EFaturaEDM.svc?singleWsdl'),
     'edm_username' => env('EDM_USERNAME', 'Datanet'),
     'edm_password' => env('EDM_PASSWORD', '1234567'),
-    
+
     'senaryo' => [
         'TICARIFATURA' => 'Ticari Fatura',
         'TEMELFATURA' => 'Temel Fatura',
     ],
-    "fatura_tur" => [
+    'fatura_tur' => [
         'SATIS' => 'Satış',
     ],
-    "cari_tur" => [
-        "TUZELKISI" => "Tüzel Kişi",
-        "GERCEKKISI" => "Gerçek Kişi",
-    ]
+    'cari_tur' => [
+        'TUZELKISI' => 'Tüzel Kişi',
+        'GERCEKKISI' => 'Gerçek Kişi',
+    ],
 ];

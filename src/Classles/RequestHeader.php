7eadb2407e6828ff6538ef6983298c389dcf7bf3
AtakanAtici <AtakanAtici@users.noreply.github.com>
2023-07-20 12:29:45 +0000

    Fix styling

--- src/Classles/RequestHeader.php
+++ src/Classles/RequestHeader.php
@@ -1,31 +1,43 @@
 <?php
+
 namespace AtakanAtici\EDM\Classes;
 
-class RequestHeader{
+class RequestHeader
+{
     public $session_id;
-    public $client_txn_id;
-    public $int_parent_txn_id;
-    public $action_date;
-    public $reason              =   "EFATURA UYGULAMA";
-    public $application_name    =   "loko";
-    public $host_name           =   "loko.hesapmen.com.tr";
-    public $channel_name        =   "HTTP";
-    public $simulation_flag     =   "N";
-    public $compressed          =   "N";
 
-    public function getArray(){
+    public $client_txn_id;
+
+    public $int_parent_txn_id;
+
+    public $action_date;
+
+    public $reason = 'EFATURA UYGULAMA';
+
+    public $application_name = 'loko';
+
+    public $host_name = 'loko.hesapmen.com.tr';
+
+    public $channel_name = 'HTTP';
+
+    public $simulation_flag = 'N';
+
+    public $compressed = 'N';
+
+    public function getArray()
+    {
         return [
-            "REQUEST_HEADER"    =>  [
-                "SESSION_ID"        =>  $this->session_id,
-                "CLIENT_TXN_ID"     =>  Util::GUID(),
-                "ACTION_DATE"       =>  Util::actionDate(),
-                "REASON"            =>  $this->reason,
-                "APPLICATION_NAME"  =>  $this->application_name,
-                "HOSTNAME"          =>  $this->host_name,
-                "CHANNEL_NAME"      =>  $this->channel_name,
-                "SIMULATION_FLAG"   =>  $this->simulation_flag,
-                "COMPRESSED"        =>  $this->compressed
-            ]
+            'REQUEST_HEADER' => [
+                'SESSION_ID' => $this->session_id,
+                'CLIENT_TXN_ID' => Util::GUID(),
+                'ACTION_DATE' => Util::actionDate(),
+                'REASON' => $this->reason,
+                'APPLICATION_NAME' => $this->application_name,
+                'HOSTNAME' => $this->host_name,
+                'CHANNEL_NAME' => $this->channel_name,
+                'SIMULATION_FLAG' => $this->simulation_flag,
+                'COMPRESSED' => $this->compressed,
+            ],
         ];
     }
 }
--- src/EDM.php
+++ src/EDM.php
@@ -4,52 +4,58 @@ namespace AtakanAtici\EDM;
 
 use AtakanAtici\EDM\Classes\Request;
 use AtakanAtici\EDM\Classes\RequestHeader;
-use phpDocumentor\Reflection\Types\Boolean;
 
 class EDM
 {
     private $err;
+
     private $service_url;
+
     private $session_id;
 
-   public function __construct($service_url)
-   {
-    $this->service_url = $service_url;
-   }
-
-    function getErr() : array
+    public function __construct($service_url)
     {
-     return $this->err;
+        $this->service_url = $service_url;
     }
 
-    function setErr($code, $message) : void {
-        $this->err = array("code" => $code, "message" => $message);
+    public function getErr(): array
+    {
+        return $this->err;
     }
 
-    function getSession() : string {
-        return $this->session_id ?? session("EFATURA_SESSION");
+    public function setErr($code, $message): void
+    {
+        $this->err = ['code' => $code, 'message' => $message];
     }
 
-    function setSession($session_id) : void {
+    public function getSession(): string
+    {
+        return $this->session_id ?? session('EFATURA_SESSION');
+    }
+
+    public function setSession($session_id): void
+    {
         $this->session_id = $session_id;
-        session(["EFATURA_SESSION" => $session_id]);
+        session(['EFATURA_SESSION' => $session_id]);
     }
 
-    function login($username, $password) : bool {
+    public function login($username, $password): bool
+    {
         $header = new RequestHeader();
-        $header->session_id = "-1";
+        $header->session_id = '-1';
         $params = $header->getArray();
-        $param["USER_NAME"] = $username;
-        $param["PASSWORD"] = $password;
+        $param['USER_NAME'] = $username;
+        $param['PASSWORD'] = $password;
         $request = new Request();
-        $session = $request->send("login", $params);
-        if ($session->SESSION_ID != "") {
+        $session = $request->send('login', $params);
+        if ($session->SESSION_ID != '') {
             $this->setSession($session->SESSION_ID);
+
             return true;
         } else {
             $this->setErr($request->hataKod, $request->hataMesaj);
+
             return false;
         }
     }
-
 }
